8f8e8c272c9f27da50a406afcc5c12c4fdfa8d0e
Veronica Toledo <vntoledo30@gmail.com>
2020-01-22 18:27:06 -0300

    Commit 1 versao do Friendly

---
+++ src/server.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const cors = require('cors');
+
+const routes = require('./routes')
+
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+/*Essa nao é a melhor maneira de ser feito
+o correto seria utilizar o conceito de chave-valor dentro do banco de dados para 
+armazenar essas informações de 'qual id de usuario é qual id de socket'*/
+const connectedUsers ={};
+
+
+
+io.on('connection', socket => {
+    const {user} = socket.handshake.query;
+
+    connectedUsers[user] = socket.id;
+
+});
+
+
+mongoose.connect(
+    process.env.MONGO_URL, 
+    {
+    useNewUrlParser: true
+    }
+);
+
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
+});
+
+
+app.use(cors());
+app.use(express.json());
+app.use(routes); //GET, POST(inserir), PUT(editar), DELETE
+
+server.listen(3333);
